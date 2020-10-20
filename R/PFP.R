50d1062b2fc0b26cdb659c9004551aef5e71d24b
AIB-KQC <kqc@sequencingdeMacBook-Pro.local>
2020-10-21 00:06:07 +0800

    commit

--- R/PFP.R
+++ R/PFP.R
@@ -6,13 +6,13 @@
 #' @docType package
 #'
 #' @importFrom graph nodes edgeL edgeData
-#' @importFrom KEGGgraph parseKGML2Graph
+#' @importFrom KEGGgraph parseKGML2Graph nodes<-
 #' @importFrom BioNet getEdgeList
 #' @importFrom clusterProfiler bitr
 #' @importFrom ggplot2 ggplot geom_point geom_segment labs theme aes element_text
-#' geom_line
+#' geom_line ylab xlab guides guide_legend
 #' @importFrom plyr llply
-#' @importFrom tidyr gather
+#' @importFrom tidyr gather %>%
 #' @importFrom stats cor p.adjust shapiro.test t.test wilcox.test
 #' @importFrom utils download.file read.csv write.csv
 NULL
